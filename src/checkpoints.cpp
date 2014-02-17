dfd93d3114e920bc7b43f12a5e9d8f7858fe03f9
Bacobob <gmail@trendcoin.com>
2014-02-17 20:46:51 +0100

    Trendcoin
    All in 1

---
+++ src/checkpoints.cpp
@@ -0,0 +1,68 @@
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2011-2012 Litecoin Developers
+// Copyright (c) 2013 TrendCoin Developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/assign/list_of.hpp> // for 'map_list_of()'
+#include <boost/foreach.hpp>
+
+#include "checkpoints.h"
+
+#include "main.h"
+#include "uint256.h"
+
+namespace Checkpoints
+{
+    typedef std::map<int, uint256> MapCheckpoints;
+
+    //
+    // What makes a good checkpoint block?
+    // + Is surrounded by blocks with reasonable timestamps
+    //   (no blocks before with a timestamp after, none after with
+    //    timestamp before)
+    // + Contains no strange transactions
+    //
+
+	// no checkpoint now, can be added in later releases
+    static MapCheckpoints mapCheckpoints =
+            boost::assign::map_list_of
+            ( 0, uint256("0xd5fa5fb98a070fd18fe707c7b4b7200977103ed8783ed0e426f093f628c3ee65"))
+            ( 1, uint256("0x440b59d53e2dfa2ae24b1a551c0d6de5f496418b3d1f6f06c7a119728b6a8656"))
+            ( 12, uint256("0x9d6954a560f7266f52721034f99db99611b122e7d6af138029dd94e01aff7078"))
+			;
+
+
+    bool CheckBlock(int nHeight, const uint256& hash)
+    {
+        if (fTestNet) return true; // Testnet has no checkpoints
+
+        MapCheckpoints::const_iterator i = mapCheckpoints.find(nHeight);
+        if (i == mapCheckpoints.end()) return true;
+        return hash == i->second;
+		// return true;
+    }
+
+    int GetTotalBlocksEstimate()
+    {
+        if (fTestNet) return 0;
+	
+        return mapCheckpoints.rbegin()->first;
+		// return 0;
+    }
+
+    CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
+    {
+        if (fTestNet) return NULL;
+
+        BOOST_REVERSE_FOREACH(const MapCheckpoints::value_type& i, mapCheckpoints)
+        {
+            const uint256& hash = i.second;
+            std::map<uint256, CBlockIndex*>::const_iterator t = mapBlockIndex.find(hash);
+            if (t != mapBlockIndex.end())
+                return t->second;
+				// return NULL;
+        }
+        return NULL;
+    }
+}
